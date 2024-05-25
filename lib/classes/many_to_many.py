c3b4b9d6e5428c22ac25f9a476a8f1313c6fbc91
Gideon Macharia <kamaugedion95@gmail.com>
2024-05-26 01:34:44 +0300

    modify code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -23,11 +23,7 @@ class Author:
     def __init__(self, name):
         self.name = name
 
-    def articles(self):
-        pass
-
-    def magazines(self):
-        pass
+   
 
     def add_article(self, magazine, title):
         pass
