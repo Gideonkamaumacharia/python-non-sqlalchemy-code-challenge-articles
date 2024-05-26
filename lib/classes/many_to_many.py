6c9b06b1cb738e8b9e6c396ed673086ffcdbd651
Gideon Macharia <kamaugedion95@gmail.com>
2024-05-26 21:56:20 +0300

    getter() setter for magazine

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -20,9 +20,11 @@ class Article:
  
 
 class Author:
+ 
     def __init__(self, name):
         self._name = name
-        self._articles = []
+        articles =[]
+        self._articles = articles
 
     @property
     def name(self):
@@ -40,19 +42,42 @@ class Author:
     def add_article(self, magazine, title):
        article = Article(self,magazine,title)
        self._articles.append(article)
-
+       
+        
     def articles(self):
         return self._articles
-        
-
 
     def topic_areas(self):
         pass
 
 class Magazine:
     def __init__(self, name, category):
+        self._name = None
+        self._category = None
         self.name = name
         self.category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and (2 <= len(value) <= 16):
+            self._name = value
+        else:
+            raise ValueError("Magazine name must be a string between 2 and 16 characters")
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+        else:
+            raise ValueError("Magazine category must be a non-empty string")
 
     def articles(self):
         pass
@@ -65,3 +90,24 @@ class Magazine:
 
     def contributing_authors(self):
         pass
+
+
+
+
+author_1 = Author("Carry Bradshaw")
+author_2 = Author("Nathaniel Hawthorne")
+magazine = Magazine("Vogue", "Fashion")
+
+
+article_1 = Article(author_1, magazine, "How to wear a tutu with style")
+author_1.add_article(article_1,"How to wear a tutu with style")
+
+article_2 = Article(author_1, magazine, "Dating life in NYC")
+author_1.add_article(article_2, "Dating life in NYC")
+
+article_3 = Article(author_2, magazine, "How to be single and happy")
+author_2.add_article(article_3,"How to be single and happy")
+
+print(article_3.__dict__)
+print(article_1.__dict__)
+print(article_2.__dict__)
